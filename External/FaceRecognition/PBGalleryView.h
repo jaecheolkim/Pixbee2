ff69a346dc403070dbb6f899347ac85cf603b189
jaecheol kim <jungwu.kim@gmail.com>
2014-04-11 15:44:58 +0900

    Camera의 갤러리뷰 작업중.

---
+++ External/FaceRecognition/PBGalleryView.h
@@ -0,0 +1,13 @@
+//
+//  PBGalleryView.h
+//  Pixbee
+//
+//  Created by jaecheol kim on 4/11/14.
+//  Copyright (c) 2014 Pixbee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBGalleryView : UIView
+
+@end
---
+++ External/FaceRecognition/PBGalleryView.m
@@ -0,0 +1,31 @@
+//
+//  PBGalleryView.m
+//  Pixbee
+//
+//  Created by jaecheol kim on 4/11/14.
+//  Copyright (c) 2014 Pixbee. All rights reserved.
+//
+
+#import "PBGalleryView.h"
+
+@implementation PBGalleryView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
